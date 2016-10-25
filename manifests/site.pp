f911dabda335e83a6bf8057069e7b763703c77b0
mwanaon <min.wan@aon.com>
2016-10-25 15:01:19 -0500

    Fix bug

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,7 @@ node default {
 
   include role::classroom
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-    path => /usr/bin:/usr/local/bin',
+    path => '/usr/bin:/usr/local/bin',
     creates => '/etc/motd',
   }
 }
