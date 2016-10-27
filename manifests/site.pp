46d35b5b5e357a0babff37e9abdaa93341e90801
mwanaon <min.wan@aon.com>
2016-10-27 11:00:15 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,6 +39,8 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
-
-
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} virtual machine.": }
+  }
 }
