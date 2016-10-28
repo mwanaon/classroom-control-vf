c0cd7568e2ca4d96185406993af23e660b21150c
mwanaon <min.wan@aon.com>
2016-10-28 10:05:32 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,6 +39,6 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {  
-  $message = heira('message')
+  $message = hiera('message')
   notify { $message: }
 }
