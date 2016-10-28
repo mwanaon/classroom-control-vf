e5d82e1e6da755507b436b7e6c3e7227a69517e5
mwanaon <min.wan@aon.com>
2016-10-28 11:41:57 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,35 +1,13 @@
 class nginx (
-  $root = undef,
+  $package = $nginx::params::package,
+  $owner = $nginx::params::owner,
+  $group = $nginx::params::group,
+  $docroot = $nginx::params::docroot,
+  $confdir = $nginx::params::confdir,
+  $logdir = $nginx::params::logdir,
+  $user = $nginx::params::user,
+  $port = $nginx::params::port
 ){
-  case $::osfamily {
-    'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-#      $docroot = '/var/www'
-      $docroot = $root
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-    }
-    'windows' : {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      #$docroot = 'C:/ProgramData/nginx/html'
-      $docroot = $root
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs'
-    }
-    default : {
-      fail("Module ${module_name} is not supported on ${::osfamily}")
-    }
-  }
-  # user the service will run as. Used in the nginx.conf.epp template
-  $user = $::osfamily ? {
-    'redhat' => 'nginx',
-    'debian' => 'www-data',
-    'windows' => 'nobody',
-  }
   File {
     owner => $owner,
     group => $group,
